db7dbdd3fb01746d30e4aacf9b867a22ea92ccea
univrsal <univrsal@protonmail.ch>
2019-12-02 23:39:46 +0100

    started work on settings tab switching

--- js/settings.js
+++ js/settings.js
@@ -1,9 +1,21 @@
-function select_tab(event)
+var active_tab_id = 1;
+var active_tab = document.getElementById('page1');
+
+function select_tab(event, id)
 {
+    document.activeElement.blur();
     var tabs = document.getElementsByClassName('tab-button');
     for (var i = 0; i < tabs.length; i++) {
         tabs[i].classList.remove('tab-button-pressed');
     }
     event.target.classList.add('tab-button-pressed');
     
+    /* switch tab */
+    var next_tab = document.getElementById('page' + id);
+    active_tab.classList.add('move-right-out');    
+    next_tab.style.display = 'block';
+    next_tab.classList.add('move-left-in');
+    next_tab.classList.add('move-align');
+
+    active_tab = next_tab;
 }
