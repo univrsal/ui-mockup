bf3e968c267d7e9ff7d3fa9184ad395d3952c4a5
Alex <univrsal@protonmail.ch>
2019-12-03 12:01:58 +0100

    finished tab switching

--- js/settings.js
+++ js/settings.js
@@ -3,6 +3,7 @@ var active_tab = document.getElementById('page1');
 
 function select_tab(event, id)
 {
+    /* Handle tab bar */
     document.activeElement.blur();
     var tabs = document.getElementsByClassName('tab-button');
     for (var i = 0; i < tabs.length; i++) {
@@ -12,10 +13,25 @@ function select_tab(event, id)
     
     /* switch tab */
     var next_tab = document.getElementById('page' + id);
-    active_tab.classList.add('move-right-out');    
+    var out_class = 'move-right-out';
+    var in_class = 'move-left-in';
+
+    if (id > active_tab_id) { /* Scrolling to the left */
+        out_class = 'move-left-out';
+        in_class = 'move-right-in';
+    }    
+
+    active_tab.classList.add(out_class);
     next_tab.style.display = 'block';
-    next_tab.classList.add('move-left-in');
-    next_tab.classList.add('move-align');
+    next_tab.classList.add(in_class);
+
+    /* Remove transition classes again */
+    setTimeout(function(in_class, out_class, next_tab, active_tab) {
+        next_tab.classList.remove(in_class);
+        active_tab.classList.remove(out_class);
+        active_tab.style.display = 'none'; 
+    }, 250, in_class, out_class, next_tab, active_tab);
 
     active_tab = next_tab;
+    active_tab_id = id;
 }
