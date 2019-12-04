77b2407df526ab2a1b927a622066b0de38163a62
univrsal <univrsal@protonmail.ch>
2019-12-04 19:48:19 +0100

    added disabled style copied over dialogs and group settings

--- js/settings.js
+++ js/settings.js
@@ -2,6 +2,27 @@ var active_tab_id = 1;
 var active_tab = document.getElementById('page1');
 var slide_block = false;
 
+function remove_class(from, name)
+{
+    from.classList.remove(name);
+}
+
+function toggle_options(event, target)
+{
+    var options = document.getElementById(target);
+    var children = options.children;
+    
+    for (var i = 0; i < children.length; i++)  {
+        if (event.target.checked) {
+            children[i].classList.remove('disabled');
+            children[i].classList.add('enabled');
+        } else {
+            children[i].classList.remove('enabled');
+            children[i].classList.add('disabled');
+        }
+    }
+}
+
 function handle_time(event)
 {
     var check = /^[0-9]{1,2}:[0-9]{1,2}$/;
@@ -14,11 +35,11 @@ function select_tab(event, id)
 {
     document.activeElement.blur();
     if (slide_block)
-        return;
+    return;
     slide_block = true;
     if (id === active_tab_id)
-        return;
-
+    return;
+    
     /* Handle tab bar */
     var tabs = document.getElementsByClassName('tab-button');
     for (var i = 0; i < tabs.length; i++) {
@@ -30,16 +51,16 @@ function select_tab(event, id)
     var next_tab = document.getElementById('page' + id);
     var out_class = 'move-right-out';
     var in_class = 'move-left-in';
-
+    
     if (id > active_tab_id) { /* Scrolling to the left */
         out_class = 'move-left-out';
         in_class = 'move-right-in';
     }    
-
+    
     active_tab.classList.add(out_class);
     next_tab.style.display = 'block';
     next_tab.classList.add(in_class);
-
+    
     /* Remove transition classes again */
     setTimeout(function(in_class, out_class, next_tab, active_tab) {
         next_tab.classList.remove(in_class);
@@ -47,7 +68,7 @@ function select_tab(event, id)
         active_tab.style.display = 'none'; 
         slide_block = false;
     }, 500, in_class, out_class, next_tab, active_tab);
-
+    
     active_tab = next_tab;
     active_tab_id = id;
 }
