3283e845120d52622f19bfaf86fe04640d6c9c5f
univrsal <univrsal@protonmail.ch>
2019-12-06 18:56:25 +0100

    license

--- js/settings.js
+++ js/settings.js
@@ -5,7 +5,7 @@
 * Copyright 2019 univrsal (universailp@web.de)
 *
 * This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as published by
+* it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, version 3 of the License.
 *
 * This program is distributed in the hope that it will be useful,
